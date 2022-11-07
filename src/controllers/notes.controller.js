bcd5e26a7be4574d2b29403fbed59c85982d2088
gabo-x86 <hurtadogabriel58@gmail.com>
2022-11-07 01:26:14 -0400

    edit note & route to edit note

--- src/controllers/notes.controller.js
+++ src/controllers/notes.controller.js
@@ -4,7 +4,7 @@ const Note = require('../models/Note'); //Importamos modelo de Nota de BD
 
 //Devuelve todas las notas
 notesCtrl.renderNotes = async(req, res) => {
-    const notes = await Note.find().lean();
+    const notes = await Note.find().lean(); //Importante añadir método lean() para que funcione
     res.render('notes/all-notes', {notes});
 }
 
@@ -20,28 +20,29 @@ notesCtrl.createNewNote = async (req, res) => {
     //req.body catchea los campos del formulario
     const {title, description} = req.body;          //Usamos destructuring para catchear los campos
     const newNote = new Note({title, description}); //Es lo mismo que {title:title, description:description}
-    await newNote.save();                                 //Guarda objeto en la BD
-    console.log(newNote);
+    await newNote.save();                           //Guarda objeto en la BD
     res.redirect('/notes');
 }
 
 
 //Pinta formulario para editar nota
-notesCtrl.renderEditForm = (req, res) => {
-    res.send('Render update-note form!');
+notesCtrl.renderEditForm = async (req, res) => {
+    const note = await Note.findById(req.params.id).lean(); //Importante añadir método lean() para que funcione
+    res.render('./notes/edit-notes', { note });             //Pasamos obj por parámetro a la vista
 }
 
 
 //Cambia valores de una nota (lo edita)
-notesCtrl.updateNote = (req, res) => {
-    res.send('Update note :P');
+notesCtrl.updateNote = async (req, res) => {
+    const { title, description } = req.body;
+    await Note.findByIdAndUpdate(req.params.id, {title:title, description:description});
+    res.redirect('/notes');
 }
 
 
 //Elimina nota
 notesCtrl.deleteNote = async (req, res) =>{
-    //req.params devuelve los params de la url enviada
-    await Note.findByIdAndDelete(req.params.id);
+    await Note.findByIdAndDelete(req.params.id); //req.params devuelve los params de la url enviada
     res.redirect('/notes');
 }
 
